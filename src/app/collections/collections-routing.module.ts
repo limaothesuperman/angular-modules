6edca4322f153301668f570dc2b70e69b1bec653
Mao Li <mli94@stevens.edu>
2023-06-07 17:14:19 -0500

    added tab menu

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
-  {path: '', component: CollectionsHomeComponent}
+  {
+    path: '', component: CollectionsHomeComponent,
+    children: [
+      {
+        path: '', component: BiographyComponent
+      },
+      {
+        path: 'companies', component: CompaniesComponent
+      },
+      {
+        path: 'partners', component: PartnersComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
@@ -11,4 +27,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class CollectionsRoutingModule { }
- 
