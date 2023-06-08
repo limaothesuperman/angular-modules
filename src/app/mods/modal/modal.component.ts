38088b468da9d9fdf7ae306d02ed7fc562624fc1
Mao Li <mli94@stevens.edu>
2023-06-08 17:13:39 -0500

    added part of modules tab

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
+
+  @Output() close = new EventEmitter();
+
+  constructor(private el: ElementRef) {
+
+  }
+
+  ngOnInit() {
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick() {
+    this.close.emit();
+  }
 
 }
