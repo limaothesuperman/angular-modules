f9185d04615c0e870082cdf59f2569402be68775
Mao Li <mli94@stevens.edu>
2023-06-09 10:47:13 -0500

    added accordion component for modules tab and finished the project.

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 export class ModsHomeComponent {
 
   modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue because it is' },
+    { title: 'What does an orange taste like?', content: 'An orange tastes like an orange' },
+    { title: 'What color is that cat?', content: 'The color of that cat is white with blended grey' }
+  ];
 
   onClick() {
     this.modalOpen = !this.modalOpen;
